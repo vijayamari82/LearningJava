9f00447769404334347427942aa7f3c15acf6b48
vijayamari82 <vijayamari82@gmail.com>
2022-05-22 11:55:53 -0400

    new test added

---
+++ src/testassignment/SampletestAssignment.java
@@ -0,0 +1,10 @@
+package testassignment;
+
+public class SampletestAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to git ");
+	}
+
+}
