08c6e861ba6e28b07835340eea8a43a17fd650a7
vijayamari82 <vijayamari82@gmail.com>
2022-05-27 19:25:52 -0400

    First commit

--- src/testassignment/LicenseIssue.java
+++ src/testassignment/LicenseIssue.java
@@ -3,26 +3,27 @@ package testassignment;
 public class LicenseIssue {
 
 	public static void main(String[] args) {
-		int age=18;
+		int age=16;
 		String currentlevel = "G";
 			
-		if (age>=18 && currentlevel.equals("G1"))
-						
-		{					
+		if (age<16) {					
+			System.out.println("you are less than 16");
+		} 
+		else if (age>=16 && currentlevel.equals("G1")){					
 			System.out.println("license issued is G1");
 		}
-		else if (age>=18 && currentlevel.equals("G2"))
-			
-		{					
+		else if (age>=16 && currentlevel.equals("G2")){					
 			System.out.println("license issued is G2 ");
 		}
-		
-        else if (age>=18 && currentlevel.equals("G"))
-			
-		{					
-			System.out.println("license issued is G ");
+		else if (age>=16 && currentlevel.equals("G")){					
+			System.out.println("you have got full G license");
+		} else {
+			System.out.println("Go for G1");
 		}
 	}
+
+
+
 	
 
 }
