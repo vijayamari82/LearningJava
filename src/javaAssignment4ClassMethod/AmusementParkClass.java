ca973e533b37a6da9d96002a643f648dd24ecef0
vijayamari82 <vijayamari82@gmail.com>
2022-06-15 00:45:54 -0400

    First commit

---
+++ src/javaAssignment4ClassMethod/AmusementParkClass.java
@@ -0,0 +1,25 @@
+package javaAssignment4ClassMethod;
+
+class AmusementParkClass //sub-class
+{
+    int height, weight;
+	void check()
+	{
+		if (height > 90 && height < 200)
+		{
+			if(weight > 15 && weight < 95)
+			{
+				System.out.println("You are allowed to Ride");
+			}
+			else  //Weight Else Block
+			{
+				System.out.println("You are not Eligble to Ride");
+			}
+		}
+		else //Height Else Block
+		{
+			System.out.println("You are not Eligble to Ride");
+		}
+	}
+}
+
